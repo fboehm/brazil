0b19110933b8cee0b8326ae71c3e92159f3f2ecd
fboehm <frederick.boehm@gmail.com>
2016-08-26 11:11:46 -0500

    added code for google API calls

---
+++ R/geocoding-call.R
@@ -0,0 +1,21 @@
+# calls to google maps API
+load("data/user_data.RData")
+source("https://raw.githubusercontent.com/LucasPuente/geocoding/master/geocode_helpers.R")
+#Install modified version of the geocode function
+#(that now includes the api_key parameter):
+source("https://raw.githubusercontent.com/LucasPuente/geocoding/master/modified_geocode.R")
+geocode_apply<-function(x){
+  geocode(x, source = "google", output = "all", api_key="AIzaSyBnR6m37FrIR7SkDj7ZPyg5pmbrL68PWFI")
+}
+# api key calling will take time, I think
+library(jsonlite)
+library(dplyr)
+library(plyr)
+#####
+#foo <- user_data$location[1:500]
+#foo <- foo[!is.na(foo)]
+#geocode_results <- sapply(foo, geocode_apply, simplify = FALSE)
+#####
+locs <- user_data$location[!is.na(user_data$location)]
+geocode_results <- sapply(locs, geocode_apply, simplify = FALSE)
+save(list = "geocode_results", "geocode_results.RData")
